9a32ca46357dc617aedb6a5a2546dbda4b8b5b7b
AssassinAsh <apple@Apples-MacBook-Pro.local>
2023-05-12 08:34:42 +0530

    added templates

--- main.go
+++ main.go
@@ -3,13 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
+
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error occurred while executing the template: ", err)
+	}
+}
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world!")
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Service started on port: %s", portNumber))
+
+	http.ListenAndServe(portNumber, nil)
 }
